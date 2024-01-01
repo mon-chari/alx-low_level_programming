7a50f2149b2c08898d468735e4ef0afc119474df
mon-chari <armonchari@gmail.com>
2024-01-01 11:28:38 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
-int main()
+
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	/*using puts*/
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
