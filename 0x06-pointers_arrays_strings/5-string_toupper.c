24879beccf05d42a8051d5100d64b9327e599a99
mon-chari <armonchari@gmail.com>
2024-01-16 03:17:59 -0800

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * string_toupper - function that changes all lowercase letters of a string to uppercase
+ * string_toupper - function that changes all lowercase letters
+ * of a string to uppercase
  * @n: String pointer
  * Return: string input
  */
