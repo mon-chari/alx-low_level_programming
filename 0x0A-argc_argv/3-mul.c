25ac101a79365c6edc02d3db5b8f05fa9564c098
mon-chari <armonchari@gmail.com>
2024-01-17 15:21:12 -0800

    0x0A. C - argc, argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include "main.h"
-/**
+
+/**              
  * _atoi - function that converts a string to an integer
  * @s: string to be converted
  * Return: the int converted from the string
  */
+
 int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
+
 	i = 0;
 	d = 0;
 	n = 0;
@@ -16,49 +19,39 @@ int _atoi(char *s)
 	digit = 0;
 
 	while (s[len] != '\0')
-	{
 		len++;
-	}
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
-		{
 			++d;
-		}
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
 		}
-		if (d % 2)
-		{
-			digit = -digit;
-		}
-		n = n * 10 + digit;
-		f = 1;
-
-		if (s[i + 1] < '0' || s[i + 1] > '9')
-			break;
-		f = 0;
+		i++;
 	}
-	i++;
-}
-if(f == 0)
-{
-	return (0);
-}
-return (n);
+	if (f == 0)
+		return (0);
+	return (n);
 }
 
-/**
- * main- function that multiplies two numbers
+/**               
+ * main - function that multiplies two numbers
  * @argc: argument count
  * @argv: argument vector
- * Return: always 0
+ * Return: 0 (Success), 1 (Error)
  */
+                
 int main(int argc, char *argv[])
 {
 	int result, num1, num2;
-
 	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
@@ -67,7 +60,6 @@ int main(int argc, char *argv[])
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
 	result = num1 * num2;
-
 	printf("%d\n", result);
 	return (0);
 }
