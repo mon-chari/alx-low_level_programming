ce74323d2be6daf670079c89b53628bd47c73fdd
mon-chari <armonchari@gmail.com>
2024-01-22 14:55:37 -0800

    3. Outside of a dog, a book is a man's best friend. Inside of a dog it's too dark to read

--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -2,3 +2,4 @@
 0.Define a new type struct dog.
 1.Write a function that initialize a variable of type struct dog
 2.Write a function that prints a struct dog
+3.Define a new type dog_t as a new name for the type struct dog
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,12 +6,15 @@
  * @age: second member
  * @owner: third member
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+void print_dog(dog_t *d);
+
 #endif
