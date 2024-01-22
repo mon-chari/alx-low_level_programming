7977ea6e434052fabc35ebb465f61bc24038eb3e
mon-chari <armonchari@gmail.com>
2024-01-22 14:49:44 -0800

    2. A dog will teach you unconditional love. If you can have that in your life, things won't be too bad

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - defines a new type
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
 struct dog
 {
 	char name[50];
-	char owner[50];
 	float age;
+	char owner[50];
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
