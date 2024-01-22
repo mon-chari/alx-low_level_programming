e2596cfcaeb179febab25d655db1c2734cb20a85
mon-chari <armonchari@gmail.com>
2024-01-22 13:00:08 -0800

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+ * struct dog - defines a new type
+ * @name: first member
+ * @owner: second member
+ * @age: third member
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
