0faa638d02b6a8ec0feefa19c9b965837dca5ef8
mon-chari <armonchari@gmail.com>
2024-01-24 12:29:26 -0800

    2. To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * int_index - function that searches for an integer
+ * @array: pointer to int
+ * @size: number of elements in the array
+ * @cmp: pointer to function
+ * Return: index of the first element
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -1,2 +1,3 @@
 0x0F. C - Function pointers
 1.Write a function that executes a function given as a parameter on each element of an array.
+2.Write a function that searches for an integer.
