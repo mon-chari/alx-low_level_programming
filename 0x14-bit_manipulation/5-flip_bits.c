67f6912452d9d0bf14a346d06030c0399e23c3ff
mon-chari <armonchari@gmail.com>
2024-02-11 08:36:31 -0800

    0x14. C - Bit manipulation

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+/**
+ * flip_bits - Returns the number of bits you would need to flip to get from
+ * one number to another.
+ * @n: The first number.
+ * @m: The second number.
+ *
+ * Return: The number of bits to flip.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int a, countbit = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
+
+	for (a = 63; a >= 0; a--)
+	{
+		current = exclusive >> a;
+		if (current & 1)
+			countbit++;
+	}
+
+	return (countbit);
+}
